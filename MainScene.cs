81ef222fdc4ecc2357eb9932e4d74de4bfb00795
mabat25 <mabat250@gmail.com>
2019-06-21 12:06:22 +0200

    minor startup adjustments

---
+++ MainScene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MBBSlib.MonoGame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectOnion
+{
+    class MainScene : IStartingPoint, MBBSlib.MonoGame.IDrawable
+    {
+        public static GameMain Game { get; set; }
+        Sprite pixel;
+        public void Start(GameMain game)
+        {
+            Game = game;
+            Game.BackgroundColor = Color.Black;
+            pixel = new Sprite("WhitePixel");
+            GameMain.RegisterRenderer(this);
+        }
+
+        public void Draw(SpriteBatch sprite)
+        {
+            sprite.Draw(pixel, new Rectangle(30, 30, 500, 500), Color.Red);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MBBSlib.MonoGame;
+using System;
 
 namespace ProjectOnion
 {
@@ -13,7 +14,7 @@ namespace ProjectOnion
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameMain(new MainScene()))
                 game.Run();
         }
     }
