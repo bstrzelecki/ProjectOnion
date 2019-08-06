365ba530e5558f5553a21bf436a8224a610935ef
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-08-06 16:49:44 +0200

    added registry

---
+++ Registry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOnion
+{
+	class Registry
+	{
+		public static List<NewFurniture> furnitures = new List<NewFurniture>();
+		public static List<Job> jobs = new List<Job>();
+		public static void Register(NewFurniture f)
+		{
+			furnitures.Add(f);
+		}
+		public static void Register(Job j)
+		{
+			jobs.Add(j);
+		}
+		public static void RegisterAll()
+		{
+
+		}
+	}
+}
