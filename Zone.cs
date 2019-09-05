872ef9318fba54e2ecaa355a82f298cb661871a0
Mabat25 <48790127+mabat25@users.noreply.github.com>
2019-09-05 18:39:10 +0200

    Zone data type

---
+++ Zone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ProjectOnion
+{
+	abstract class Zone
+	{
+		protected Vector2 position;
+		protected Tile tile;
+		public Zone(Tile tile)
+		{
+			this.tile = tile;
+			position = tile.Position;
+		}
+		public abstract Job GetZoneJob();
+	}
+}
